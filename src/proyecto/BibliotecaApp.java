2af8621dce0c1f336db6843d455d8bb0c9446ee1
Mei-2513 <j-uliana9@hotmail.com>
2023-09-23 18:34:19 -0500

    funcionalidad para agregar libros en la clase BibliotecaApp

--- src/proyecto/BibliotecaApp.java
+++ src/proyecto/BibliotecaApp.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class BibliotecaApp extends JFrame {
+
     private Biblioteca biblioteca = new Biblioteca();
 
     public BibliotecaApp() {
@@ -18,55 +19,69 @@ public class BibliotecaApp extends JFrame {
         agregarLibroButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JTextField tituloField = new JTextField(20);
-                JTextField isbnField = new JTextField(20);
-                JTextField volumenField = new JTextField(5);
-                JTextField editorialField = new JTextField(20);
-                JTextField nombreAutorField = new JTextField(20);
-                JTextField apellidoAutorField = new JTextField(20);
-                JTextField biografiaAutorField = new JTextField(20);
-                JTextField nombreSedeField = new JTextField(20);
-                JTextField cantidadCopiasField = new JTextField(5);
+                try {
+                    JTextField tituloField = new JTextField(20);
+                    JTextField isbnField = new JTextField(20);
+                    JTextField volumenField = new JTextField(5);
+                    JTextField editorialField = new JTextField(20);
+                    JTextField nombreAutorField = new JTextField(20);
+                    JTextField apellidoAutorField = new JTextField(20);
+                    JTextField biografiaAutorField = new JTextField(20);
+                    JTextField nombreSedeField = new JTextField(20);
+                    JTextField cantidadCopiasField = new JTextField(5);
 
-                JPanel inputPanel = new JPanel();
-                inputPanel.setLayout(new GridLayout(9, 2));
-                inputPanel.add(new JLabel("Título del libro:"));
-                inputPanel.add(tituloField);
-                inputPanel.add(new JLabel("ISBN del libro:"));
-                inputPanel.add(isbnField);
-                inputPanel.add(new JLabel("Volumen del libro:"));
-                inputPanel.add(volumenField);
-                inputPanel.add(new JLabel("Editorial del libro:"));
-                inputPanel.add(editorialField);
-                inputPanel.add(new JLabel("Nombre del autor:"));
-                inputPanel.add(nombreAutorField);
-                inputPanel.add(new JLabel("Apellido del autor:"));
-                inputPanel.add(apellidoAutorField);
-                inputPanel.add(new JLabel("Biografía del autor:"));
-                inputPanel.add(biografiaAutorField);
-                inputPanel.add(new JLabel("Nombre de la sede:"));
-                inputPanel.add(nombreSedeField);
-                inputPanel.add(new JLabel("Cantidad de copias:"));
-                inputPanel.add(cantidadCopiasField);
+                    JPanel inputPanel = new JPanel();
+                    inputPanel.setLayout(new GridLayout(9, 2));
+                    inputPanel.add(new JLabel("Título del libro:"));
+                    inputPanel.add(tituloField);
+                    inputPanel.add(new JLabel("ISBN del libro:"));
+                    inputPanel.add(isbnField);
+                    inputPanel.add(new JLabel("Volumen del libro:"));
+                    inputPanel.add(volumenField);
+                    inputPanel.add(new JLabel("Editorial del libro:"));
+                    inputPanel.add(editorialField);
+                    inputPanel.add(new JLabel("Nombre del autor:"));
+                    inputPanel.add(nombreAutorField);
+                    inputPanel.add(new JLabel("Apellido del autor:"));
+                    inputPanel.add(apellidoAutorField);
+                    inputPanel.add(new JLabel("Biografía del autor:"));
+                    inputPanel.add(biografiaAutorField);
+                    inputPanel.add(new JLabel("Nombre de la sede:"));
+                    inputPanel.add(nombreSedeField);
+                    inputPanel.add(new JLabel("Cantidad de copias:"));
+                    inputPanel.add(cantidadCopiasField);
 
-                int result = JOptionPane.showConfirmDialog(null, inputPanel, "Agregar Libro",
-                        JOptionPane.OK_CANCEL_OPTION);
-                if (result == JOptionPane.OK_OPTION) {
-                    String titulo = tituloField.getText();
-                    String isbn = isbnField.getText();
-                    int volumen = Integer.parseInt(volumenField.getText());
-                    String editorial = editorialField.getText();
-                    String nombreAutor = nombreAutorField.getText();
-                    String apellidoAutor = apellidoAutorField.getText();
-                    String biografiaAutor = biografiaAutorField.getText();
-                    String nombreSede = nombreSedeField.getText();
-                    int cantidadCopias = Integer.parseInt(cantidadCopiasField.getText());
+                    int result = JOptionPane.showConfirmDialog(null, inputPanel, "Agregar Libro",
+                            JOptionPane.OK_CANCEL_OPTION);
+                    if (result == JOptionPane.OK_OPTION) {
+                        String titulo = tituloField.getText();
+                        String isbn = isbnField.getText();
+                        int volumen = Integer.parseInt(volumenField.getText());
+                        String editorial = editorialField.getText();
+                        String nombreAutor = nombreAutorField.getText();
+                        String apellidoAutor = apellidoAutorField.getText();
+                        String biografiaAutor = biografiaAutorField.getText();
+                        String nombreSede = nombreSedeField.getText();
+                        int cantidadCopias = Integer.parseInt(cantidadCopiasField.getText());
 
-                    Autor autor = new Autor(nombreAutor, apellidoAutor, biografiaAutor);
-                    Libro libro = new Libro(titulo, isbn, volumen, editorial, autor, biblioteca.getSede(nombreSede), cantidadCopias);
+                        Autor autor = new Autor(nombreAutor, apellidoAutor, biografiaAutor);
+                        Sede sede = biblioteca.getSede(nombreSede);
 
-                    biblioteca.agregarLibro(libro);
-                    JOptionPane.showMessageDialog(null, "Libro agregado con éxito.");
+                        if (sede != null) {
+                            Libro libro = new Libro(titulo, isbn, volumen, editorial, autor, sede, cantidadCopias);
+                            biblioteca.agregarLibro(libro);
+                            JOptionPane.showMessageDialog(null, "Libro agregado con éxito.");
+                        } else {
+                            JOptionPane.showMessageDialog(null, "La sede especificada no existe.",
+                                    "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                    }
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Error: Ingresa valores numéricos válidos.", "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, "Error inesperado: " + ex.getMessage(),
+                            "Error", JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
@@ -89,5 +104,3 @@ public class BibliotecaApp extends JFrame {
         });
     }
 }
-
-
