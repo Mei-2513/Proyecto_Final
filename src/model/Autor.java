4a6a11cd119f87ad3465358201ced1204349a244
Mei-2513 <j-uliana9@hotmail.com>
2023-09-23 15:02:14 -0500

    Contenido clase Autor

--- src/model/Autor.java
+++ src/model/Autor.java
@@ -1,5 +1,20 @@
 package model;
 
 public class Autor {
+	
+	String nombre;
+	String apelliddo;
+	String biografia;
+	
+	
+	public Autor(String nombre, String apelliddo, String biografia) {
+		
+		this.nombre = nombre;
+		this.apelliddo = apelliddo;
+		this.biografia = biografia;
+	}
+	
+	
+	
 
 }
