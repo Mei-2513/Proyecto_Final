4b8ec10dda70628260ee23c24f2385079b8dd5db
Mei-2513 <j-uliana9@hotmail.com>
2023-09-23 15:06:51 -0500

    Contenido clase  Libro

--- src/model/Libro.java
+++ src/model/Libro.java
@@ -1,5 +1,28 @@
 package model;
 
 public class Libro {
+	
+	String titulo;
+	String ISBN;
+	int volumen;
+	String editorial;
+	Autor autor;
+	Sede sede;
+	int cantidadCopias;
+	
+	
+	public Libro(String titulo, String iSBN, int volumen, String editorial, Autor autor, Sede sede,
+			int cantidadCopias) {
+	
+		this.titulo = titulo;
+		ISBN = iSBN;
+		this.volumen = volumen;
+		this.editorial = editorial;
+		this.autor = autor;
+		this.sede = sede;
+		this.cantidadCopias = cantidadCopias;
+	}
+	
+	
 
 }
