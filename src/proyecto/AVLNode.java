41afe62f1448e19c15ce82140ddf1c4763fdc349
Mei-2513 <j-uliana9@hotmail.com>
2023-09-23 15:16:18 -0500

    Contenido de la clase AVLNode

--- src/proyecto/AVLNode.java
+++ src/proyecto/AVLNode.java
@@ -1,5 +1,20 @@
 package proyecto;
 
 public class AVLNode {
+	
+	Libro libro;
+	int altura;
+	AVLNode izquierdo;
+	AVLNode derecho;
+	
+	
+	public AVLNode(Libro libro) {
+		super();
+		this.libro = libro;
+		this.altura = 1;
+		this.izquierdo = null;
+		this.derecho = null;
+	}
+	
 
 }
