bb37d1e49a1665052c153d2c7350bc18a9690993
Mei-2513 <j-uliana9@hotmail.com>
2023-09-23 16:05:37 -0500

    contenido nuevo clase biblioteca

--- src/proyecto/Biblioteca.java
+++ src/proyecto/Biblioteca.java
@@ -1,6 +1,23 @@
 package proyecto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Biblioteca {
+	
+	private AVLTree arbolLibros= new AVLTree();
+	private Map<String, Sede> sedes = new HashMap<>();
+	
+	
+
+	public Biblioteca() {
+		
+		sedes.put(null, null);
+		sedes.put(null, null);
+		
+	}
+
+
 
 	public static void main(String[] args) {
 		
